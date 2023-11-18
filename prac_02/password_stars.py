1a7d1028a4094c529ac16008c5a913839f05b5a0
ygaop <yi.gao2@my.jcu.edu.au>
2023-11-18 14:59:15 +0800

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,26 @@
+"""Password Check with Functions"""
+
+
 MIN_LENGTH = 8
 
-password = input("Password: ")
-while len(password) < MIN_LENGTH:
-    print("Your password is too short.")
+
+def main():
+    """Function docstring"""
+    # statements...
+    password = get_password("Your password is too short")
+    print_password(password)
+
+
+def get_password(error):
     password = input("Password: ")
-print("*" * len(password))
+    while len(password) < MIN_LENGTH:
+        print(error)
+        password = input("Password: ")
+    return password
+
+
+def print_password(password):
+    print("*" * len(password))
+
+
+main()
