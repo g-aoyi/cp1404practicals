f972ead9341b4c862d2fd4fa91a3f66a972a556f
ygaop <yi.gao2@my.jcu.edu.au>
2023-12-11 15:29:59 +0800

    Add prac 6 files

---
+++ prac_06/guitar.py
@@ -0,0 +1,21 @@
+"""CP1404/CP5632 Practical"""
+
+
+class Guitar:
+
+    def __init__(self, name, year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} ({self.year}) : ${self.cost:,}"
+
+    def get_age(self):
+        YEAR = 2023
+        age = YEAR - self.year
+        return age
+
+    def is_vintage(self):
+        age = self.get_age()
+        return age >= 50
